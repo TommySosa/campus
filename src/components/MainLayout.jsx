d7ac72cf160fad651f4c8aa2d4eabd7e572cd7a0
TommySosa <tomas.facundo.sosa@gmail.com>
2023-09-04 17:55:13 -0300

    conditional rendering for teacher page

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -9,8 +9,7 @@ import Image from 'next/image'
 import { useSession } from 'next-auth/react'
 const navigation = [
   { name: 'Inicio', href: '/home' },
-  { name: 'Cursos', href: '/courses' },
-  { name: 'Profesor', href: '/teacher' }
+  { name: 'Cursos', href: '/courses' }
 ]
 
 function classNames(...classes) {
@@ -22,6 +21,7 @@ export default function MainLayout() {
   const { data: session, status } = useSession();
   const loginRoute = pathname === '/auth/login'
   const registerRoute = pathname === '/auth/register'
+  const teacherRoute = pathname === '/teacher'
 
   return (
     <Disclosure as="nav" className="bg-elf-green-600">
@@ -68,6 +68,24 @@ export default function MainLayout() {
                       )
                     }
                     )}
+                    {
+                      session ? (
+                        <>
+                          {
+                            session.user.id_rol === 2 ? (
+                              <>
+                                <Link href={'/teacher'} className={classNames(
+                                  teacherRoute ? 'bg-elf-green-800 text-white' : 'text-gray-300 hover:bg-elf-green-700 hover:text-white',
+                                  'block rounded-md px-3 py-2 text-base font-medium'
+                                )}>
+                                  Profesor
+                                </Link>
+
+                              </>
+                            ) : null
+                          }
+                        </>) : null
+                    }
                     {
                       !session ? (
                         <>
@@ -150,64 +168,9 @@ export default function MainLayout() {
                         </Menu.Item>
                       </Menu.Items>
                     </Menu></>) : (
-                    // <div className='flex space-x-4'>
-                    //   <Link href={'/auth/login'}>
-                    //     <button>Iniciar sesión</button>
-                    //   </Link>
-                    //   <Link href={'/auth/register'}>
-                    //     <button>Registrarse</button>
-                    //   </Link>
-
-                    // </div>
                     null
                   )
                 }
-
-                {/* <Menu as="div" className="relative ml-3">
-                <div>
-                  <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                    <span className="absolute -inset-1.5" />
-                    <span className="sr-only">Open user menu</span>
-                    <Image
-                      className="h-8 w-8 rounded-full"
-                      src={defaultImage}
-                      alt=""
-                    />
-                  </Menu.Button>
-                </div>
-                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        <Menu.Item>
-                          {({ active }) => (
-                            <Link
-                              href="/user"
-                              className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                            >
-                              Mi perfil
-                            </Link>
-                          )}
-                        </Menu.Item>
-                        <Menu.Item>
-                          {({ active }) => (
-                            <Link
-                              href="/user/settings"
-                              className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                            >
-                              Configuración
-                            </Link>
-                          )}
-                        </Menu.Item>
-                        <Menu.Item>
-                          {({ active }) => (
-                            <Link
-                              href="/api/auth/signout"
-                              className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                            >
-                              Cerrar sesión
-                            </Link>
-                          )}
-                        </Menu.Item>
-                      </Menu.Items>
-                    </Menu> */}
               </div>
             </div>
           </div>
@@ -233,6 +196,24 @@ export default function MainLayout() {
                   )
                 )
               })}
+              {
+                session ? (
+                  <>
+                    {
+                      session.user.id_rol === 2 ? (
+                        <>
+                          <Link href={'/teacher'} className={classNames(
+                            teacherRoute ? 'bg-elf-green-800 text-white' : 'text-gray-300 hover:bg-elf-green-700 hover:text-white',
+                            'block rounded-md px-3 py-2 text-base font-medium'
+                          )}>
+                            Profesor
+                          </Link>
+
+                        </>
+                      ) : null
+                    }
+                  </>) : null
+              }
               {
                 !session ? (
                   <>
