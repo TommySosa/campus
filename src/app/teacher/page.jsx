555007d1c7c813615950b51b7a17d39600d8d318
TommySosa <tomas.facundo.sosa@gmail.com>
2023-09-04 17:20:07 -0300

    Buttons cards in teacher page

--- src/app/teacher/page.jsx
+++ src/app/teacher/page.jsx
@@ -1,17 +1,28 @@
-import Link from "next/link"
+import ButtonsCard from "@/components/ButtonsCard"
 
 export default function Home() {
   return (
     <>
-      <p>Teacher page</p>
-      <Link href="/teacher/create-exercises">
-        Crear ejercicios
-      </Link>
-
-      <Link href="/teacher/create-course">
-        Crear curso
-      </Link>
-      
+      <ButtonsCard title="Cursos" firstButton={{color: "blue", label: "Crear", href:"/teacher/create-course"}}
+      secondButton={{color: "green", label: "Editar", href:"/teacher/edit-course"}}
+      thirdButton={{color: "red", label: "Borrar", href:"/teacher/delete-course"}}
+      fourthButton={{color: "yellow", label: "Ver", href:"/teacher/view-course"}}
+      />
+      <ButtonsCard title="Ejercicios" firstButton={{color: "blue", label: "Crear", href:"/teacher/create-exercise"}}
+      secondButton={{color: "green", label: "Editar", href:"/teacher/edit-exercise"}}
+      thirdButton={{color: "red", label: "Borrar", href:"/teacher/delete-exercise"}}
+      fourthButton={{color: "yellow", label: "Ver", href:"/teacher/view-exercise"}}
+      />
+      <ButtonsCard title="Alumnos" firstButton={{color: "blue", label: "Asignar cursos", href:"/teacher/create-student"}}
+      secondButton={{color: "green", label: "Cambiar cursos", href:"/teacher/edit-student"}}
+      thirdButton={{color: "red", label: "Dar de baja", href:"/teacher/delete-student"}}
+      fourthButton={{color: "yellow", label: "Ver", href:"/teacher/view-student"}}
+      />
+      <ButtonsCard title="Profesores" firstButton={{color: "blue", label: "Asignar cursos", href: "/teacher/create-teacher"}}
+      secondButton={{color: "green", label: "Cambiar cursos", href: "/teacher/edit-teacher"}}
+      thirdButton={{color: "red", label: "Dar de baja", href: "/teacher/delete-teacher"}}
+      fourthButton={{color: "yellow", label: "Ver", href: "/teacher/view-teacher"}}
+      />
     </>
   )
 }
