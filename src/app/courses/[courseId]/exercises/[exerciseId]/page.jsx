50dd8706a36503502d906d78906813c666db69c6
TommySosa <tomas.facundo.sosa@gmail.com>
2023-09-13 15:14:30 -0300

    rendering multiple choise options

--- src/app/courses/[courseId]/exercises/[exerciseId]/page.jsx
+++ src/app/courses/[courseId]/exercises/[exerciseId]/page.jsx
@@ -1,7 +1,11 @@
+import Multiple from "@/components/Multiple";
+
 export default async function Home({ params }) {
     const response = await fetch(`http://localhost:3000/api/exercises/${params.exerciseId}`);
     const data = await response.json();
     const exercise = data.data;
+
+    console.log('DATA', exercise);
 
     if (data.data.error || exercise === null || !response.ok) {
         return (
@@ -12,11 +16,29 @@ export default async function Home({ params }) {
         );
     }
 
+    // if(multipleData.data.error || multiple === null || !multipleResponse.ok) {
+    //     return (
+    //         <div>
+    //             <h1>Ejercicio no encontrado</h1>
+    //             <p>Lo sentimos, el ejercicio que estás buscando no existe.</p>
+    //         </div>
+    //     );
+    // }
     return (
         <main>
             <div>
-                <h1>Bienvenido al curso {exercise.name}!</h1>
+                <h1>{exercise.name}</h1>
                 <p>{exercise.instruction}</p>
+            </div>
+            <div>
+                {
+                    exercise.id_type === 1 ? 
+                    (
+                        <Multiple id={params.exerciseId}/>
+                    ) : <p>No es un multiple choise</p>
+                }
+                
+                
             </div>
         </main>
     );
--- src/components/CollapsableButtonCourse.jsx
+++ src/components/CollapsableButtonCourse.jsx
@@ -4,11 +4,36 @@ import ModuleButton from './ModuleButton';
 
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
-const CollapsableButtonCourse = ({id_params}) => {
-    const { data, error, isLoading } = useSWR(`/api/modules/${id_params}`, fetcher);
+export default async function CollapsableButtonCourse({id_params}) {
+    // const { data, error, isLoading } = useSWR(`/api/modules/${id_params}`, fetcher);
 
-    if (error) return <div>failed to load</div>
-    if (isLoading) return <Spinner />
+    const response = await fetch(`/api/modules/${id_params}`)
+    const data = await response.json();
+
+    // fetch(`/api/modules/${id_params}`)
+    //     .then(async response => {
+    //         const data = await response.json()
+
+    //         return (
+    //             <div>
+    //                 {
+    //                 data.data.length > 0 ? (
+    //                     data.data.map((module) => (
+    //                         <ModuleButton key={module.id_module} module={module}/>
+    //                     ))
+    //                 ) : <div>No hay módulos</div>
+    //                 }
+    //             </div>
+    //         );
+    //     })
+    //     .catch(error => {
+    //         console.log(error);
+    //         return <div>failed to load</div>
+    //     });
+    
+
+    // if (error) return <div>failed to load</div>
+    // if (isLoading) return <Spinner />
 
     return (
         <div>
@@ -24,4 +49,3 @@ const CollapsableButtonCourse = ({id_params}) => {
 
 };
 
-export default CollapsableButtonCourse;
