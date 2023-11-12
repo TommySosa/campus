8dde1dc59ba2e845af51a48d650a22892bef7e74
TommySosa <tomas.facundo.sosa@gmail.com>
2023-11-12 19:58:18 -0300

    cambios en el archivo sql

--- query_database.sql
+++ query_database.sql
@@ -88,16 +88,6 @@ create table multiple_choise(
     foreign key(id_exercise) references exercises(id_exercise)
 );
 
--- Insertar un ejercicio de opción múltiple con opciones en formato JSON
--- INSERT INTO exercises (exercise_id, id_type) VALUES (1, 1); -- Ejemplo, donde 1 es el ID del tipo de ejercicio
--- INSERT INTO multiple_choice (exercise_id, options) VALUES (1, '[{"text": "Opción A", "correct": true}, {"text": "Opción B", "correct": false}, {"text": "Opción C", "correct": false}]');
-
--- Consulta para obtener ejercicio de opción múltiple y sus opciones
--- SELECT e.id_exercise, m.options
--- FROM exercises e
--- INNER JOIN multiple_choice m ON e.id_exercise = m.id_exercise
--- WHERE e.id_exercise = 1; -- Cambiar el ID del ejercicio según sea necesario
-
 create table true_or_false(
 	id_exercise int,
     true_option varchar(250) not null,
@@ -124,61 +114,6 @@ create table drag_options(
     foreign key(id_exercise) references drag_and_drop(id_exercise)
 );
 
--- Insertar ejercicio de arrastrar y soltar en tabla drag_and_drop
--- INSERT INTO drag_and_drop (exercise_id, statement) VALUES (1, 'Arrastra las opciones correctas al lugar correcto.');
-
--- Insertar opciones arrastrables en tabla drag_options para el ejercicio con exercise_id 1
--- INSERT INTO drag_options (exercise_id, text) VALUES (1, 'Opción 1');
--- INSERT INTO drag_options (exercise_id, text) VALUES (1, 'Opción 2');
--- INSERT INTO drag_options (exercise_id, text) VALUES (1, 'Opción 3');
--- Puedes agregar más opciones aquí
-
-
-select * from users;
-insert into categories(name) values ('English');
-insert into courses(name,description,id_category) values ('Curso 1','Descripcion del curso 1', 1);
-insert into progress(correct_exercises, incorrect_exercises, total_exercises, id_course, id_user) values(23,7, 30, 1, 1);
-insert into modules(name,id_course) values('Modulo del curso 2', 2);
-insert into exercise_types(name) values('Verdadero o falso');
-insert into exercises(name,instruction,id_module,id_type) values("Verb to be 1", "Realiza el sig. ejercicio", 1, 1);
-insert into exercises(name,instruction,id_module,id_type) values("He-She-It", "Como se dice él", 1, 2);
-select * from exercises;
-INSERT INTO multiple_choise (id_exercise, options) VALUES (1, '[{"text": "Opción A", "correct": true}, {"text": "Opción B", "correct": false}, {"text": "Opción C", "correct": false}]');
-INSERT INTO multiple_choise (id_exercise, options) VALUES (3, '[{"text": "Opción A", "correct": false}, {"text": "Opción B", "correct": true}, {"text": "Opción C", "correct": false}]');
-
-select * from categories;
-select * from courses;
-select * from progress;
-select * from modules;
-select * from exercise_types;
-select * from exercises;
-select * from multiple_choise;
-select * from true_or_false;
-insert into true_or_false(id_exercise,true_option, false_option) values(4,'He', 'She');
-
-SELECT exercises.*, exercise_types.name AS exercise_type
-FROM exercises
-INNER JOIN exercise_types ON exercises.id_type = exercise_types.id_type;
-
-
-SELECT 
-    exercises.*, 
-    exercise_types.name AS exercise_type,
-    multiple_choise.options AS multiple_choice_options,
-    true_or_false.true_option,
-    true_or_false.false_option,
-    drag_and_drop.instruction AS drag_and_drop_instruction,
-    GROUP_CONCAT(drag_options.text) AS drag_options_texts
-FROM exercises
-INNER JOIN exercise_types ON exercises.id_type = exercise_types.id_type
-LEFT JOIN multiple_choise ON exercises.id_exercise = multiple_choise.id_exercise
-LEFT JOIN true_or_false ON exercises.id_exercise = true_or_false.id_exercise
-LEFT JOIN drag_and_drop ON exercises.id_exercise = drag_and_drop.id_exercise
-LEFT JOIN drag_options ON drag_and_drop.id_exercise = drag_options.id_exercise
-GROUP BY exercises.id_exercise;
-
-
--- Crea las tablas students y teachers
 CREATE TABLE students (
     id_user int not null unique,
     
@@ -193,7 +128,6 @@ CREATE TABLE teachers (
     foreign key (id_user) references users(id_user)
 );
 
--- Crear la tabla intermedia para estudiantes y cursos
 CREATE TABLE student_courses (
     id_student_course int not null unique auto_increment,
     id_student int not null,
@@ -204,48 +138,22 @@ CREATE TABLE student_courses (
     foreign key(id_course) references courses(id_course)
 );
 
-
-ALTER TABLE users
-ADD COLUMN id_rol INT,
-ADD CONSTRAINT fk_users_roles
-FOREIGN KEY (id_rol)
-REFERENCES roles(id_rol);
-
+insert into roles(name) values ("student");
 insert into roles(name) values ("teacher");
-select * from roles;
-
+insert into categories(name) values ('English');
+insert into courses(name,description,id_category) values ('Curso 1','Descripcion del curso 1', 1);
+insert into modules(name,id_course) values('Modulo del curso 1', 1);
+insert into exercise_types(name) values('Multiple Choise');
+insert into exercise_types(name) values('Verdadero o falso');
+insert into exercises(name,instruction,id_module,id_type) values("Verb to be 1", "Realiza el sig. ejercicio", 1, 1);
+insert into exercises(name,instruction,id_module,id_type) values("He-She-It", "Como se dice él", 1, 2);
+INSERT INTO multiple_choise (id_exercise, options) VALUES (1, '[{"text": "Opción A", "correct": true}, {"text": "Opción B", "correct": false}, {"text": "Opción C", "correct": false}]');
+INSERT INTO multiple_choise (id_exercise, options) VALUES (3, '[{"text": "Opción A", "correct": false}, {"text": "Opción B", "correct": true}, {"text": "Opción C", "correct": false}]');
+insert into true_or_false(id_exercise,true_option, false_option) values(4,'He', 'She');
 UPDATE users SET id_rol = (2) WHERE id_user = 1;
-UPDATE courses set id_teacher = (1) WHERE id_course = 2;
 
-ALTER TABLE courses
-ADD COLUMN id_teacher int;
 
-ALTER TABLE courses
-ADD CONSTRAINT fk_teacher
-FOREIGN KEY (id_teacher)
-REFERENCES teachers(id_user);
 
-SELECT
-    c.name AS course_name,
-    CONCAT(u.name, ' ', u.surname) AS teacher_name,
-    cat.name AS category_name
-FROM
-    courses AS c
-INNER JOIN
-    users AS u ON c.id_teacher = u.id_user
-INNER JOIN
-    categories AS cat ON c.id_category = cat.id_category
-WHERE
-    c.id_course = 1;
-    
-select * from modules;
 
-SELECT modules.*, courses.name as course_name 
-FROM modules 
-JOIN courses ON modules.id_course = courses.id_course where id_course = 1;
-
-SELECT * FROM exercises WHERE id_course = 2 AND id_exercise = 1;
-
-SELECT * FROM multiple_choise WHERE id_exercise = 2;
 
 
